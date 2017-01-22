b81104d8ce6d757187ea2732d9dc8bcb2abce47d
FelisDJ <J_Jackthip@hotmail.com>
2017-01-22 14:24:19 +0700

    Initial Commit

---
+++ PhotoEncUI/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PhotoEncUI
+//
+//  Created by FelisDJ on 1/22/2560 BE.
+//  Copyright © 2560 Jackthip Pureesatian. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
